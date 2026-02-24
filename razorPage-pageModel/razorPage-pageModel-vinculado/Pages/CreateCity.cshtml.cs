9d5f2908e25fe040641947b058ffa9e2ce257e3c
GUSTAVO <gustavofont10@gmail.com>
2026-02-24 20:43:24 -0300

    Implementando ex 2 e 3 - validação de input e tratamento de erros.

--- razorPage-pageModel/razorPage-pageModel-vinculado/Pages/CreateCity.cshtml.cs
+++ razorPage-pageModel/razorPage-pageModel-vinculado/Pages/CreateCity.cshtml.cs
@@ -1,18 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace razorPage_pageModel_vinculado.Pages
 {
     public class CreateCityModel : PageModel
     {
-        public string CityName { get; set; }
-        public void OnPost(string cityName) 
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public string CityNameResult { get; set; }
+
+        public void OnPost()
         {
-            if (!string.IsNullOrEmpty(cityName))
+            if (ModelState.IsValid)
             {
-                this.CityName = cityName;
+                this.CityNameResult = Input.CityName;
             }
         }
 
+        public class InputModel
+        {
+            [Required(ErrorMessage = "O preenchimento do nome é obrigatório.")]
+            [MinLength(3, ErrorMessage = "A cidade precisa de pelo menos 3 caracteres.")]
+            public string CityName { get; set; }
+        }
     }
 }
